68f19e932959415aa6a501eb53f94abf60642224
mohanbendegumbali <mohan0311@hotmail.com>
2022-10-27 09:58:26 +0530

    [Mohan] Add: Length Calculated

---
+++ LineComparision/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; 
+
+namespace LineComparision 
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine(" Welcome to Line Comparision problem statement");
+            CalculateLength calculate = new CalculateLength(1, 2, 3, 4);
+            calculate.Calculate();
+        }
+    }
+}
