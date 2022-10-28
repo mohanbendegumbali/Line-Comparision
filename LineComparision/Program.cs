a5edafcce5ac5b53ba00ad0af6d93a519ee95825
mohanbendegumbali <mohan0311@hotmail.com>
2022-10-28 08:37:21 +0530

    [Mohan] Add: Folder structure Created

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -7,8 +7,15 @@ namespace LineComparision
         public static void Main(string[] args) 
         {
             Console.WriteLine(" Welcome to Line Comparision problem statement");
-            CalculateLength calculate = new CalculateLength(1, 2, 3, 4);
-            calculate.Calculate();
+            CalculateLength calculate1 = new CalculateLength(1, 2, 3, 4);
+            double length1 = calculate1.Calculate();
+            CalculateLength calculate2 = new CalculateLength(1, 2, 3, 4);
+            double length2 = calculate2.Calculate();
+            if (length1.Equals(length2))
+                Console.WriteLine(" Both are equal");
+            else
+                Console.WriteLine(" Both are not equal");
+                    
         }
     }
 }
